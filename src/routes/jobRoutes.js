d642f6b82773f7adf24fc2ced7294b48f188972b
YtalloAlves <ytollo.alves@academico.ifpb.edu.br>
2024-05-23 14:37:41 -0300

    feat: adicionando rota para a view de adicionar vagas

--- src/routes/jobRoutes.js
+++ src/routes/jobRoutes.js
@@ -7,6 +7,12 @@ router.get('/test', (req, res) =>{
     res.send('deu certo')
 })
 
+
+//rota teste
+router.get('/add', (req, res) =>{
+    res.render('add')
+})
+
 //adiciona uma vaga via post
 router.post('/add',(req, res) => {
     let{title, description, salary, company, email, new_job} = req.body
