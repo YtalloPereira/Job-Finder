fd897323a284c9bb516b09b9a826e81ab000d649
YtalloAlves <ytollo.alves@academico.ifpb.edu.br>
2024-05-24 12:13:44 -0300

    feat: Adicionando rota para carregar os detalhes de uma vaga

--- src/routes/jobRoutes.js
+++ src/routes/jobRoutes.js
@@ -7,8 +7,18 @@ router.get('/test', (req, res) =>{
     res.send('deu certo')
 })
 
+//detalhes da vaga > view/1 > view/2
+router.get('/view/:id', (req, res) => Job.findOne({
+    where: {id: req.params.id}
+    }).then(job =>{
+        res.render('view',{
+            job
+        })
+    }).catch(err => console.log(err))
+)
 
-//busca do formulário
+
+//form da rota de envio
 router.get('/add', (req, res) =>{
     res.render('add')
 })
