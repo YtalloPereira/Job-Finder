6fd8baf9209bfbd82bdc25db8176209bb4969cef
YtalloAlves <ytollo.alves@academico.ifpb.edu.br>
2024-05-24 09:27:12 -0300

    fix: corrigindo rota de recuperar todos os jobs

--- src/app.js
+++ src/app.js
@@ -1,21 +1,17 @@
 //Configurando express
 const express = require('express')
 const app = express()
-
 //importando handlebars
 const exphbs = require('express-handlebars');
-
 //importando o banco
 const db = require('./db/connexion')
-
 //setando body parser
 const bodyParser = require('body-parser')
-
 //Setando a porta
 const PORT = 3000
-
 const path = require('path')
-
+//Pegando todos os jobs
+const Job  = require('./models/Job');
 
 //Ouvindo a porta
 app.listen(PORT,function(){
@@ -29,16 +25,25 @@ db.authenticate()
 .catch(err => {
     console.log('Ocorreu um erro ao conectar', err)
 })
-//Testando se a porta funciona(routes)
+//Pegando todas as vagas e ordenando em ordem decrescente de criação
+//Renderizando a view com as vagas nela
 app.get('/',(req, res) =>{
-    res.send('index')
+    Job.findAll({order: [
+        ['createdAt', 'DESC']
+    ]})
+    .then(jobs =>{
+        res.render('index',{
+            jobs
+        })
+    })
 })
+
 
 //utilizando body parser
 app.use(bodyParser.urlencoded({ extended: false}))
 
 //handlebars
-app.set('views', path.join(__dirname, 'src/views')) 
+app.set('views', path.join(__dirname, '/views')) 
 app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 
